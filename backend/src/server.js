f45abade32a15a67d34ca71d37c0705754e00d2c
Setulal Chowhan <setulalchowhan@gmail.com>
2025-10-26 12:50:52 +0600

    done connect to database

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import path from "path";
+import { connectDB } from "./lib/db.js"; 
 
 const app = express();
 
@@ -23,6 +24,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
