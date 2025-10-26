0baecaf58db2f650fcbe3ca2381e7b5a519915fb
Setulal Chowhan <setulalchowhan@gmail.com>
2025-10-26 11:36:06 +0600

    initial commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const login =async(req,res)=>{
+    
+}
+export const signup =async(req,res)=>{
+
+}
+export const logout =async(req,res)=>{
+
+}
+export const updateProfile =async(req,res)=>{
+
+}
