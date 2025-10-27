22ee9e405979614d40523f4439db989f63e04bb7
Setulal Chowhan <setulalchowhan@gmail.com>
2025-10-27 13:57:52 +0600

    feat: add protectRoute middleware for route protection

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
-// import { protectRoute } from "../middleware/auth.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 // import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
@@ -11,7 +11,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.put("/update-profile", updateProfile);
+router.put("/update-profile",protectRoute, updateProfile);
 
 // router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
